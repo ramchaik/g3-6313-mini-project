c72e4503586d060abd193c0f5f573ac68df37e23
Vaibhav Singh <vb777222@dal.ca>
2024-08-08 21:21:00 -0300

    feat: add approve and upload project contract methods

--- eth/CryptoArt.sol
+++ eth/CryptoArt.sol
@@ -6,6 +6,8 @@ contract CryptoArt {
         address proposer;
         address artist;
         string ipfsCid;
+        string progressIpfsCid;
+        string artIpfsCid;
         uint256 totalMilestones;
         uint256 completedMilestones;
         uint256 totalPayment;
@@ -20,12 +22,23 @@ contract CryptoArt {
     event ProjectCreated(uint256 projectId, address proposer, string ipfsCid);
     event ProjectAccepted(uint256 projectId, address artist);
     event ProgressUpdated(uint256 projectId, string newIpfsCid);
-    event MilestoneCompleted(uint256 projectId, uint256 milestoneNumber, uint256 payment);
+    event MilestoneCompleted(
+        uint256 projectId,
+        uint256 milestoneNumber,
+        uint256 payment
+    );
     event ProjectCompleted(uint256 projectId);
 
     // Function to create a new project and store the IPFS CID
-    function createProject(string memory _ipfsCid, uint256 _totalMilestones, uint256 _totalPayment) public payable {
-        require(msg.value == _totalPayment, "Payment must match total project cost");
+    function createProject(
+        string memory _ipfsCid,
+        uint256 _totalMilestones,
+        uint256 _totalPayment
+    ) public payable {
+        require(
+            msg.value == _totalPayment,
+            "Payment must match total project cost"
+        );
         projectCount++;
         projects[projectCount] = Project({
             proposer: msg.sender,
@@ -35,67 +48,99 @@ contract CryptoArt {
             completedMilestones: 0,
             totalPayment: _totalPayment,
             releasedPayment: 0,
-            isCompleted: false
+            isCompleted: false,
+            progressIpfsCid: "",
+            artIpfsCid: ""
         });
         emit ProjectCreated(projectCount, msg.sender, _ipfsCid);
     }
 
     // Function to read the details of a project by ID
-    function readProject(uint256 _projectId) public view returns (Project memory) {
+    function readProject(
+        uint256 _projectId
+    ) public view returns (Project memory) {
         return projects[_projectId];
     }
 
     // Function to update the IPFS CID of a project
-    function updateProject(uint256 _projectId, string memory _newIpfsCid) public {
-        require(msg.sender == projects[_projectId].proposer, "Only proposer can update project");
+    function updateProject(
+        uint256 _projectId,
+        string memory _newIpfsCid
+    ) public {
+        require(
+            msg.sender == projects[_projectId].proposer,
+            "Only proposer can update project"
+        );
         projects[_projectId].ipfsCid = _newIpfsCid;
         emit ProgressUpdated(_projectId, _newIpfsCid);
     }
 
     // Function to delete a project
     function deleteProject(uint256 _projectId) public {
-        require(msg.sender == projects[_projectId].proposer, "Only proposer can delete project");
-        require(!projects[_projectId].isCompleted, "Cannot delete completed project");
+        require(
+            msg.sender == projects[_projectId].proposer,
+            "Only proposer can delete project"
+        );
+        require(
+            !projects[_projectId].isCompleted,
+            "Cannot delete completed project"
+        );
         delete projects[_projectId];
     }
 
     // Function for the artist to accept the project contract
     function acceptProjectContract(uint256 _projectId) public {
-        require(projects[_projectId].artist == address(0), "Project already accepted");
+        require(
+            projects[_projectId].artist == address(0),
+            "Project already accepted"
+        );
         projects[_projectId].artist = msg.sender;
         emit ProjectAccepted(_projectId, msg.sender);
     }
 
     // Function for the artist to update the progress of the project
-    function updateProgress(uint256 _projectId, string memory _newIpfsCid) public {
-        require(msg.sender == projects[_projectId].artist, "Only artist can update progress");
-        projects[_projectId].ipfsCid = _newIpfsCid;
+    function updateProgress(
+        uint256 _projectId,
+        string memory _newIpfsCid
+    ) public {
+        Project storage project = projects[_projectId];
+        require(
+            msg.sender == project.artist,
+            "Only artist can update progress"
+        );
+
+        project.progressIpfsCid = _newIpfsCid;
         emit ProgressUpdated(_projectId, _newIpfsCid);
     }
 
-    // Function to complete a milestone and release payment
-    function milestoneComplete(uint256 _projectId) public {
-        Project storage project = projects[_projectId];
-        require(msg.sender == project.proposer, "Only proposer can complete milestone");
-        require(project.completedMilestones < project.totalMilestones, "All milestones already completed");
-        
+    function approveProgress(
+        uint256 _projectId,
+        string memory _newIpfsCid
+    ) public {
+        Project memory project = projects[_projectId];
+        require(
+            msg.sender == project.proposer,
+            "Only proposer can approve project"
+        );
+
+        // Signed progress document by the proper
+        project.artIpfsCid = _newIpfsCid;
+
         project.completedMilestones++;
         uint256 payment = project.totalPayment / project.totalMilestones;
         project.releasedPayment += payment;
-        
+
         payable(project.artist).transfer(payment);
-        emit MilestoneCompleted(_projectId, project.completedMilestones, payment);
-        
+        emit MilestoneCompleted(
+            _projectId,
+            project.completedMilestones,
+            payment
+        );
+
         if (project.completedMilestones == project.totalMilestones) {
             project.isCompleted = true;
             emit ProjectCompleted(_projectId);
         }
-    }
-
-    // Function to validate the progress of the project
-    function validateProgress(uint256 _projectId) public view returns (bool) {
-        Project memory project = projects[_projectId];
-        return (project.completedMilestones > 0 && !project.isCompleted);
     }
 
     // Function to get the total number of projects
@@ -104,12 +149,23 @@ contract CryptoArt {
     }
 
     // Function to recover the signer's address from the signature
-    function recoverSigner(bytes32 messageHash, uint8 v, bytes32 r, bytes32 s) public pure returns (address) {
+    function recoverSigner(
+        bytes32 messageHash,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public pure returns (address) {
         return ecrecover(messageHash, v, r, s);
     }
 
     // Function to verify the document signature by comparing the recovered address to the artist's address
-    function verifySignature(uint256 _projectId, bytes32 messageHash, uint8 v, bytes32 r, bytes32 s) public view returns (bool) {
+    function verifySignature(
+        uint256 _projectId,
+        bytes32 messageHash,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public view returns (bool) {
         Project memory project = projects[_projectId];
         address recoveredAddress = ecrecover(messageHash, v, r, s);
         return (recoveredAddress == project.artist);
